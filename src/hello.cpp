4e8191f84e30ab9df793bf028b9c417ecdd48ff4
Jeff H <hough552@gmail.com>
2022-09-08 10:44:38 -0400

    added better examples for cmake.

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,7 +1,10 @@
 #include <fmt/format.h>
 #include <iostream>
+#include "../include/adder_library.h"
 
 int main()
 {
-  std::cout << fmt::format("{}, {}", "Hello", "World") << std::endl;
+  double a = 1.02;
+  double b = -3.5;
+  std::cout << fmt::format("The sum of {} and {} is: {}", a, b, math::add(a, b)) << std::endl;
 }
